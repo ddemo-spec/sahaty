8b0eec3d3ef6bd85100f2825ba720ba10ece0fdf
Mishel Hanna <mishelhanna3@gmail.com>
2024-08-09 04:45:02 +0300

    r

--- sahaty/settings.py
+++ sahaty/settings.py
@@ -26,6 +26,12 @@ SECRET_KEY = 'django-insecure-_db@m1#ilt9pfa#0f2w70(&1skikgm9#h31nj2n)9=2d3%eg&r
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+    "http://localhost:12345"
+]
 
 
 
@@ -82,13 +88,33 @@ WSGI_APPLICATION = 'sahaty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': getenv('PGDATABASE'),
+#     'USER': getenv('PGUSER'),
+#     'PASSWORD': getenv('PGPASSWORD'),
+#     'HOST': getenv('PGHOST'),
+#     'PORT': getenv('PGPORT'),
+#     'OPTIONS': {
+#       'sslmode': 'require',
+#     },
+#   }
+# }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': "sahatydb",
+    'USER': "sahatydb_owner",
+    'PASSWORD': "9CONvj6nDTQx",
+    'HOST': "ep-crimson-bar-a55v66s9.us-east-2.aws.neon.tech",
+    'PORT': 5432,
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -126,7 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [] 
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
